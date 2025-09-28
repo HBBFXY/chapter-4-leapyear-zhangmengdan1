35ab1d498b076cb558dcabfb48c4f5f6ee6ca236
zhangmengdan1 <2037228815@qq.com>
2025-09-28 21:04:43 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-year = int(input("请输入一个年份："))
-if (year % 400 == 0) or (year % 4 == 0 and year % 100 != 0):
-    print(f"{year}是闰年。")
-else:
-    print(f"{year}不是闰年。")
+try:
+    year = int(input("请输入年份："))
+    if (year % 4 == 0 and year % 100 != 0) or year % 400 == 0:
+        print(f"{year}年是闰年")
+    else:
+        print(f"{year}年不是闰年")
+except ValueError:
+    print("输入错误")
 
